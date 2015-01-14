a0ddd6e11a9b8d3b922e4c07b8238b4cca1d450f
akrigline <andrew@akrigline.com>
2015-01-14 12:26:01 -0500

    getting rid of independent js callout

--- js/main.js
+++ js/main.js
@@ -39,12 +39,10 @@ var backgroundSize = function() {
 	        "background-size": "auto 100%"
 	    })
 	}
-}
+};
 
 $(document).ready(backgroundSize);
-
 $(window).resize(backgroundSize);
-
 
 (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
 (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
